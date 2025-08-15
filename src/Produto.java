6515d26cff0b9cbda3d6438d621c040c8b4952df
fic <fic@NOTE02-S28>
2025-08-14 21:39:42 -0300

    atualizacao de comentarios

--- src/Produto.java
+++ src/Produto.java
@@ -3,14 +3,25 @@ public class Produto {
     // 1. atributo
     // public tipo nome
     //tamanhp, preco, cor, unidade, marca, nome
-    public String nome;
-    public double tamanho;
-    public double preco;
-    public String cor;
-    public int unidade;
-    public String marca;
+    private String nome;
+    private double tamanho;
+    private double preco;
+    private String cor;
+    private int unidade;
+    private String marca;
 
+    //getters setters
+    // get - pegar
 
+    //public String getNome(){
+    // return nome;
+    // }
+
+    //set - definir, mudar
+
+    //public void setNome(String nome){
+    // this.nome = nome;
+    // }
 
     //2. Metodo
     // mostra informacao do produto
